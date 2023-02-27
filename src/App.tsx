afe3cb8ea928f7b4d1c3738e4c6a5b08b9eaf285
Artem M <artemmarin91@mail.ru>
2023-02-27 04:38:29 +0300

    configured routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header';
+import Catalog from './pages/Catalog/Catalog'
+import Product from './pages/Product/Product'
 
 const App: FC = () => (
   <div className="App">
-    <header className="App-header">
-      <p>hello there</p>
-    </header>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Catalog />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
   </div>
 );
 
