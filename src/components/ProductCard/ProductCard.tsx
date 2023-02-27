3180cae74154c8110a1b1d712731ae0a8a3e9c49
Artem M <artemmarin91@mail.ru>
2023-02-27 04:42:00 +0300

    added ProductCard Component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { IProduct } from '../../types'
+import './ProductCard.css'
+
+const ProductCard = (product: IProduct) => {
+  const { colors, name, id } = product
+  const genLink = () => `/product?id=${id}`
+
+  return (
+    <Link to={genLink()} className="card">
+      <span className="title">{name}</span>
+      <img className="img" alt="фото продукта" src={colors[0].images[0]} />
+    </Link>
+  );
+}
+
+export default ProductCard
