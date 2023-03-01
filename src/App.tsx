a68b62652ab8b138b0427779e5d4615126e34f18
Artem M <artemmarin91@mail.ru>
2023-03-01 03:31:12 +0300

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import Catalog from './pages/Catalog/Catalog'
 import Product from './pages/Product/Product'
 
 const App: FC = () => (
-  <div className="App">
+  <>
     <Header />
     <Routes>
       <Route path="/" element={<Catalog />} />
       <Route path="/product" element={<Product />} />
     </Routes>
-  </div>
+  </>
 );
 
 export default App;
--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -19,36 +19,39 @@ const ProductInfo: FC<IProps> = ({ setSelectedColorIndex, selectedColorIndex }) 
 
   return (
     <div className="product-info">
-      <h2>{currentProduct?.name}</h2>
-      <span>{currentColor?.description}</span>
-      <span>Цена: {currentColor?.price}</span>
-      <div className="product-info__btn-group">
-        {currentProduct?.colors.map((color, i) => (
-          <button
-            key={color.id}
-            type="button"
-            onClick={() => setSelectedColorIndex(i)}
-            className={`btn-group__btn ${selectedColorIndex === i && 'btn-group__btn_active'}`}
-          >
-            {color.name}
-          </button>
-        ))}
-      </div>
-      <div className="product-info__btn-group">
-        {availableSizes?.length ? (
-          availableSizes.map((size) => (
+      <div className="product-info__wrapper">
+
+        <h2>{currentProduct?.name}</h2>
+        <span>{currentColor?.description}</span>
+        <span>Цена: {currentColor?.price}</span>
+        <div className="product-info__btn-group">
+          {currentProduct?.colors.map((color, i) => (
             <button
-              key={size}
+              key={color.id}
               type="button"
-              onClick={() => setCurrentSize(size)}
-              className={`btn-group__btn ${currentSize === size && 'btn-group__btn_active'}`}
+              onClick={() => setSelectedColorIndex(i)}
+              className={`btn-group__btn ${selectedColorIndex === i && 'btn-group__btn_active'}`}
             >
-              {allSizes?.find((s) => s.id === size)?.label}
+              {color.name}
             </button>
-          ))
-        ) : (
-          <span>Все размеры закончились</span>
-        )}
+          ))}
+        </div>
+        <div className="product-info__btn-group">
+          {availableSizes?.length ? (
+            availableSizes.map((size) => (
+              <button
+                key={size}
+                type="button"
+                onClick={() => setCurrentSize(size)}
+                className={`btn-group__btn ${currentSize === size && 'btn-group__btn_active'}`}
+              >
+                {allSizes?.find((s) => s.id === size)?.label}
+              </button>
+            ))
+          ) : (
+            <span>Все размеры закончились</span>
+          )}
+        </div>
       </div>
     </div>
   );
